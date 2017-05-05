917d200205d537258c8afc713ab913f5d68733b4
董宸 <352070977@qq.com>
2017-05-06 00:07:40 +0800

    稍微修改了一些东西

--- src/main/java/com/scienjus/smartqq/Application.java
+++ src/main/java/com/scienjus/smartqq/Application.java
@@ -13,37 +13,34 @@ import java.util.List;
  */
 public class Application {
 
+    private static SmartQQClient client;
     public static void main(String[] args) {
-        //创建一个新对象时需要扫描二维码登录，并且传一个处理接收到消息的回调，如果你不需要接收消息，可以传null
-        SmartQQClient client = new SmartQQClient(new MessageCallback() {
+
+        MessageCallback callback = new MessageCallback() {
             @Override
             public void onMessage(Message message) {
-                System.out.println(message.getContent());
+                System.out.println("onMessage：" + message.getContent());
             }
 
             @Override
             public void onGroupMessage(GroupMessage message) {
-                System.out.println(message.getContent());
+                System.out.println("onGroupMessage：" + message.getContent());
+                ResponseGroup(client, message);
             }
 
             @Override
             public void onDiscussMessage(DiscussMessage message) {
-                System.out.println(message.getContent());
+                System.out.println("onDiscussMessage：" + message.getContent());
             }
-        });
-        //登录成功后便可以编写你自己的业务逻辑了
-        List<Category> categories = client.getFriendListWithCategory();
-        for (Category category : categories) {
-            System.out.println(category.getName());
-            for (Friend friend : category.getFriends()) {
-                System.out.println("————" + friend.getNickname());
-            }
-        }
-        //使用后调用close方法关闭，你也可以使用try-with-resource创建该对象并自动关闭
-        try {
-            client.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        };
+
+        client = new SmartQQClient(callback);
+
+    }
+
+    public static void ResponseGroup(SmartQQClient client, GroupMessage message){
+        if(message.getContent().equals("命大")){
+            client.sendMessageToGroup(message.getGroupId(), "啊？");
         }
     }
 }
